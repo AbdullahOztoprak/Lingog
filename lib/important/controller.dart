8924322700b2b6535a59aa00852a37985b018cf8
AbdullahOztoprak <e210503006@stud.tau.edu.tr>
2025-01-28 15:58:21 +0300

    asdasd

--- lib/important/controller.dart
+++ lib/important/controller.dart
@@ -16,7 +16,7 @@ class Controller extends ChangeNotifier {
   int amount = 2000;
   int totalEarnings = 0;
 
-  // keysMap harflerin durumlarını tutan bir map olmal
+  // keysMap harflerin durumlarını tutan bir map olmalı
   Map<String, KeyboardKeyState> keysMap = {};
 
   // Klavye durumlarını başlat
